57f4ee148fd1d6595c2e2c0f0bb98e1758b0d946
Fadi Muhammad <ffadi14@gmail.com>
2019-09-08 16:40:11 +0300

    add routing

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './src/components/App';
+import AppRouter from './src/routers/Router'
 
-ReactDOM.render(<App />, document.querySelector("#container"));
+ReactDOM.render(<AppRouter />, document.querySelector("#container"));
