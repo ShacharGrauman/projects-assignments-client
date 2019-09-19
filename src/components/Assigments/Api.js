7e301da271cdbd60a4bdeb6ce551c1a4247dc8ef
Jeries Zamel <Zamel.65@gmail.com>
2019-09-19 16:14:16 +0300

    add Assignment works

--- src/components/Assigments/Api.js
+++ src/components/Assigments/Api.js
@@ -1,38 +1,60 @@
-import axios from 'axios';
-const url="http://localhost:8080/api/"
-const pageNumberLimit="pageNumber=1&limit=5"
-class Api{
+import axios from "axios";
+const url = "http://localhost:8080/api/";
+const pageNumberLimit = "pageNumber=1&limit=5";
+class Api {
+  async getProjects() {
+    const projects = await axios.get(`${url}projects/manager/1`);
+    return projects.data;
+  }
+  async getProjectsByProjectName(projectName) {
+    const projects = await axios.get(
+      `${url}projects/name/${projectName}?${pageNumberLimit}`
+    );
+    return projects.data;
+  }
+  async getProjectsByEmployeeName(employeeName) {
+    const projects = await axios.get(
+      `${url}projects/user/name/${employeeName}`
+    );
+    return projects.data;
+  }
+  async getEmpForProjects(projectID) {
+    const employees = await axios.get(`${url}team/project/${projectID}`);
+    return employees.data;
+  }
+  async getPendingAssignments(projectID) {
+    const employees = await axios.get(
+      `${url}assignments/request/1?pageNumber=1&limit=10`
+    );
+    return employees.data;
+  }
+  async sendAssignment(status, assignID) {
+    // console.log(assignID)
+    const assign = await axios.post(
+      `${url}assignments/status?response=${status}`,
+      { id: assignID }
+    );
+    // console.log(assign)
+    return assign.status === 200;
+  }
+  async getMyTeam() {
+    const employees = await axios.get(`${url}team/1/?&pageNumber=1&limit=10`);
+    return employees.data;
+  }
+  async addNewAssignment(employeeID,projectID,requestFromManagerID,requestToManagerID) {
+    // console.log(assignID)
+    const newAssign = await axios.post(
+      `${url}assignments`,
+      {
+        employeeID: employeeID,
+        projectID: projectID,
+        requestFromManagerID: requestFromManagerID,
+        requestToManagerID: requestToManagerID
+      }
+    );
     
-    async getProjects(){
-        const projects = await axios.get(`${url}projects/manager/1`);
-        return projects.data;
-    }
-    async getProjectsByProjectName(projectName){
-        const projects = await axios.get(`${url}projects/name/${projectName}?${pageNumberLimit}`);
-        return projects.data;
-
-    }
-    async getProjectsByEmployeeName(employeeName){
-        const projects = await axios.get(`${url}projects/user/name/${employeeName}`);
-        return projects.data;
-
-    }
-    async getEmpForProjects(projectID){
-        const employees = await axios.get(`${url}team/project/${projectID}`);
-        return employees.data;
-
-    }
-    async getPendingAssignments(projectID){
-        const employees = await axios.get(`${url}assignments/request/1?pageNumber=1&limit=10`);
-        return employees.data;
-
-    }
-    async sendAssignment(status,assignID){
-        const assign = await axios.post(` `);
-        return assign.status===200;
-
-    }
-
+    return newAssign.status === 200;
+  }
 }
 
 export default new Api();
