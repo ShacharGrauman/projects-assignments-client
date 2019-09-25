fa9e76fce50d5ed3eb3b6734d54694a328f9f16d
Rannem Daher <raneem.dz34@gmail.com>
2019-09-25 16:31:38 +0300

    last merging for my team page

--- src/components/Assigments/MyTeamDetailsTable.js
+++ src/components/Assigments/MyTeamDetailsTable.js
@@ -1,45 +1,43 @@
 import React from "react";
-import Pagination from '../users-list/Pagination'
+import { Link } from "react-router-dom";
 import SkillBadge from "./SkillBadge";
-import { noAuto } from "@fortawesome/fontawesome-svg-core";
 
 export default class MyTeamDetailsTable extends React.Component {
   render() {
     return (
       <>
-
-
-        <div className="d-flex justify-content-center align-items-center mb-4 mt-3">
-          <div className="col-2 col-md-8 col-sm-4 col-lg-8 " style={{paddingRight: '50%'}}>
-          <table 
+        <div
+          className="row justify-content-center"
+        >
+          <div className="col-10 col-md-8 col-sm-4 col-lg-8 ">
+          <table
             className="table "
             style={{
-              //justifyContent:'center',
-              margin: "auto",
-              display: "block",
               border: "1px solid black",
-              width: '1200px'
+              width: '100%'
             }}
           >
             <thead className="thead-dark">
               <tr>
-                <th scope="col" style={{width: '20%'}}>Name</th>
-                <th scope="col" style={{width: '10%'}}>Employee Number</th>
-                <th scope="col" style={{width: '30%'}}>Technical Skills</th>
-                <th scope="col" style={{width: '30%'}}>Product Skills</th>
-                <th scope="col" style={{width: '10%'}}>Actions</th>
+              <th scope="col">Name</th>
+                <th scope="col">Employee Number</th>
+                <th scope="col">Technical Skills</th>
+                <th scope="col">Product Skills</th>
+                <th scope="col">Actions</th>
               </tr>
             </thead>
             <tbody>
               {this.props.employees.map(employee => {
                 return (
                   <tr key={employee.id}>
-                    <td>
+                    <td style={{width: '20%'}}>
                       {/* <img src={employee.img} style={{ width: "50px" }}></img> */}
                       <a href="#">{employee.name}</a>
                     </td>
-                    <td>{employee.id}</td>
-                    <td>
+
+                    <td style={{width: '10%'}}>{employee.id}</td>
+
+                    <td style={{width: '30%'}}>
                       {employee.technicalSkills.map((skill, index) => {
                         return (
                           <SkillBadge
@@ -51,7 +49,7 @@ export default class MyTeamDetailsTable extends React.Component {
                         );
                       })}
                     </td>
-                    <td>
+                    <td style={{width: '30%'}}>
                       {employee.productSkills.map((skill, index) => {
                         return (
                           <SkillBadge
@@ -63,7 +61,7 @@ export default class MyTeamDetailsTable extends React.Component {
                         );
                       })}
                     </td>
-                    <td>
+                    <td style={{width: '10%'}}>
                       <button
                         type="button"
                         className="btn btn-primary"
@@ -86,9 +84,6 @@ export default class MyTeamDetailsTable extends React.Component {
               })}
             </tbody>
           </table>
-          <div>
-            <Pagination />
-          </div>
           </div>
           
         </div>
